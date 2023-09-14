931a4c4bf4267dd39869ee2f0af052b81b10b474
norphel <norphel317@gmail.com>
2023-09-14 14:30:59 +0530

    tweak minor UI issues

--- dropdownMenu/index.js
+++ dropdownMenu/index.js
@@ -2,6 +2,7 @@ const title = document.querySelector(".title");
 title.addEventListener("click", () => {
   const listItems = document.querySelector(".listItems");
   const dropdownArrow = document.querySelector('img[alt="dropdown arrow"]');
+  title.classList.toggle("active");
   listItems.classList.toggle("active");
   dropdownArrow.classList.toggle("active");
 });
